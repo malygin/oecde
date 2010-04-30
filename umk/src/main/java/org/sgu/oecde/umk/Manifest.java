dbd8103452dc1b65a984bbb9585a4043e2dd1905
Basakov <basakovvy@info.sgu.ru>
2010-04-30 11:58:55 +0000

    git-svn-id: file:///var/www/svn2/oecdo/oecde@508 50b15a66-aedb-45cf-97b2-84293224aabc

---
+++ umk/src/main/java/org/sgu/oecde/umk/Manifest.java
@@ -0,0 +1,16 @@
+package org.sgu.oecde.umk;
+
+import org.ieee.ltsc.xsd.lom.Lom;
+import org.imsglobal.xsd.imscp_v1p1.OrganizationsType;
+import org.imsglobal.xsd.imscp_v1p1.ResourcesType;
+
+/**
+ * @author Basakov
+ */
+public class Manifest {
+    private Lom metadata;
+    private ResourcesType resources;
+    private OrganizationsType organizations;
+
+
+}
