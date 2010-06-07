0a58995ef55e70496401f8faeeb1f3bb6b142cd8
Malygin Andrey <malygin@info.sgu.ru>
2010-06-07 10:11:40 +0000

    добавление  модуля "сообщения" в основной проект
    git-svn-id: file:///var/www/svn2/oecdo/oecde@579 50b15a66-aedb-45cf-97b2-84293224aabc

---
+++ messages/src/main/java/org/sgu/oecde/messages/Message.java
@@ -0,0 +1,13 @@
+
+package org.sgu.oecde.messages;
+
+import org.sgu.oecde.core.BasicItem;
+
+/**
+ * @author Andrey Malygin (mailto: anmalygin@gmail.com)
+ * created 07.06.2010
+ * 
+ */
+public class Message extends BasicItem {
+
+}
