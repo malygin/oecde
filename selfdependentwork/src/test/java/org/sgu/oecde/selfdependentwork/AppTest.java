034f09de40d77bf231e6035f016dec27157a5543
Shihov Michael <shihovmy@info.sgu.ru>
2010-06-15 13:20:50 +0000

    git-svn-id: file:///var/www/svn2/oecdo/oecde@602 50b15a66-aedb-45cf-97b2-84293224aabc

--- selfdependentwork/src/test/java/org/sgu/oecde/selfdependentwork/AppTest.java
+++ selfdependentwork/src/test/java/org/sgu/oecde/selfdependentwork/AppTest.java
@@ -11,7 +11,7 @@ import static org.junit.Assert.*;
 /**
  * Unit test for simple App.
  */
-@ContextConfiguration(locations={"../applicationContext.xml","../spring/cwBeans.xml","../spring/testBeans.xml"})
+@ContextConfiguration(locations={"../applicationContext.xml","../spring/cwBeans.xml"})
 public class AppTest extends BasicTest{
 
     @Ignore
@@ -19,13 +19,13 @@ public class AppTest extends BasicTest{
     public void equal(){
         SelfDependentWork w = new SelfDependentWork();
         Curriculum c = new Curriculum();
-        c.setId(2);
+        c.setId(2L);
         w.setCurriculum(c);
-        w.setStudent(new Student(1));
+        w.setStudent(new Student(1L));
         w.setNumber(1);
         SelfDependentWork w2 = new SelfDependentWork();
         w2.setCurriculum(c);
-        w2.setStudent(new Student(1));
+        w2.setStudent(new Student(1L));
         w2.setNumber(1);
         assertTrue(w.equals(w2));
     }
