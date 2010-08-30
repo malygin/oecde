5a26f97ae73bb1b163752b5493e283e65b68e08a
Shihov Michael <shihovmy@info.sgu.ru>
2010-08-30 13:35:40 +0000

    git-svn-id: file:///var/www/svn2/oecdo/oecde@748 50b15a66-aedb-45cf-97b2-84293224aabc

--- news/src/main/java/org/sgu/oecde/news/dao/NewsDAO.java
+++ news/src/main/java/org/sgu/oecde/news/dao/NewsDAO.java
@@ -21,6 +21,8 @@ public class NewsDAO extends BasicDao<NewsItem> implements INewsDao{
         super(NewsItem.class);
     }
 
+    private static final long serialVersionUID = 160L;
+
     /**
      * {@inheritDoc }
      */
