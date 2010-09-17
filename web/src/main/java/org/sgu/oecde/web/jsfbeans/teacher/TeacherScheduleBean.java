1a10dce626b15404b0b631f4f51e62f1019ca4b0
Shihov Michael <shihovmy@info.sgu.ru>
2010-09-17 07:56:18 +0000

    git-svn-id: file:///var/www/svn2/oecdo/oecde@806 50b15a66-aedb-45cf-97b2-84293224aabc

---
+++ web/src/main/java/org/sgu/oecde/web/jsfbeans/teacher/TeacherScheduleBean.java
@@ -0,0 +1,46 @@
+package org.sgu.oecde.web.jsfbeans.teacher;
+
+import java.util.List;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.ViewScoped;
+import org.sgu.oecde.schedule.Lesson;
+import org.sgu.oecde.schedule.dao.ILessonDao;
+
+/**
+ *
+ * @author ShihovMY
+ */
+@ManagedBean(name="teacherScheduleBean")
+@ViewScoped
+public class TeacherScheduleBean extends AbstractTeacherBean{
+
+    @ManagedProperty(value="#{lessonDao}")
+    ILessonDao lessonDao;
+
+    List<Lesson>lessons;
+
+    @ManagedProperty(value="#{teacherSessionBean}")
+    protected TeacherSessionBean teacherSessionBean;
+
+    private static final long serialVersionUID = 151L;
+
+    public List<Lesson> getSchedule(){
+        if(lessons==null){
+            Lesson ex = new Lesson();
+            ex.setTeacher(teacher);
+            ex.setWinter(semesterGetter.getCurrentSemester()==1);
+            ex.setYear(semesterGetter.getCurrentYear());
+            lessons = lessonDao.getByExample(ex);
+        }
+        return lessons;
+    }
+
+    public void setLessonDao(ILessonDao lessonDao) {
+        this.lessonDao = lessonDao;
+    }
+
+    public void setTeacherSessionBean(TeacherSessionBean teacherSessionBean) {
+        this.teacherSessionBean = teacherSessionBean;
+    }
+}
