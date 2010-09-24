8fb45638facada9915984be200cb7e06d0ffd0cb
Malygin Andrey <malygin@info.sgu.ru>
2010-09-24 10:45:19 +0000

    компонента редактор-поле и дата-пикер(еще не компонента) в test.xhtml пример!
    git-svn-id: file:///var/www/svn2/oecdo/oecde@819 50b15a66-aedb-45cf-97b2-84293224aabc

---
+++ web/src/main/webapp/resources/tiny_mce/tinymce_init.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+tinyMCE.init({
+    mode : "specific_textareas",
+    theme : "simple",
+    debug : true,
+    editor_selector : "tinymce"
+});
