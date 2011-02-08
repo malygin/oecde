b9e34a58ec3441fefb4cba6b9cdbf01f3e98ba0f
Malygin Andrey <malygin@info.sgu.ru>
2011-02-08 09:47:36 +0000

    конвертер для красивового вывода дат
    git-svn-id: file:///var/www/svn2/oecdo/oecde@927 50b15a66-aedb-45cf-97b2-84293224aabc

---
+++ web/src/main/java/org/sgu/oecde/web/jsfbeans/util/DateConverter.java
@@ -0,0 +1,67 @@
+
+package org.sgu.oecde.web.jsfbeans.util;
+
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
+
+/**
+ * @author Andrey Malygin (mailto: anmalygin@gmail.com)
+ * created 08.02.2011
+ * конфертер который приводит дату вида 2009.01.01 13:23:12 к виду "1 января 13:23"
+ */
+@FacesConverter( value="dateConverter" )
+public class DateConverter implements Converter, Serializable {
+    private static Map<String, String> map=new HashMap<String, String>();
+      static{
+        map.put("01", "января");
+        map.put("02", "февраля");
+        map.put("03", "марта");
+        map.put("04", "апреля");
+        map.put("05", "мая");
+        map.put("06", "июня");
+        map.put("07", "июля");
+        map.put("08", "августа");
+        map.put("09", "сентября");
+        map.put("10", "октября");
+        map.put("11", "ноября");
+        map.put("12", "декабря");
+      }
+
+    public Object getAsObject(FacesContext facesContext, UIComponent component, String string) {
+         Enum object = null;
+
+        return object;
+    }
+
+    public String getAsString(FacesContext facesContext, UIComponent component, Object object) {
+       if (object == null) {
+            return null;
+        }else{
+           StringBuffer dateResult= new StringBuffer();
+           String[] date=((String)object).split(" ");
+           if (date[0]!=null){
+               String[] ymd=date[0].split("\\.");
+               dateResult.append((ymd[2].charAt(0)=='0')?ymd[2].charAt(1):ymd[2])
+                       .append(" ").append(map.get(ymd[1]));
+               if(date.length>1){
+                   String[] hms=date[1].split(":");
+                   dateResult.append(" "+hms[0]+":"+hms[1]);
+               }
+           }
+
+           return dateResult.toString();
+        }
+    }
+
+}
