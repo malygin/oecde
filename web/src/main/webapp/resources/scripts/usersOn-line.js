2e68f553ef5fcc30474950ee74ced016c9ed75d3
Malygin Andrey <malygin@info.sgu.ru>
2011-07-13 18:57:37 +0000

    последние изменения перед переездом
    git-svn-id: file:///var/www/svn2/oecdo/oecde@1179 50b15a66-aedb-45cf-97b2-84293224aabc

--- web/src/main/webapp/resources/scripts/usersOn-line.js
+++ web/src/main/webapp/resources/scripts/usersOn-line.js
@@ -1,17 +1,21 @@
 $(document).ready(function(){
-   sdfsdf("ul.dropdown li");
+    hiddenBlocks("ul.dropdown li");
 });
 
 
-function sdfsdf(sdfsd){
-     $(sdfsd).click(
-      function() {
-          if ($(this).find(".usersOnlineTableContainer").css("display") == 'none') {
-              $(this).css('list-style-image', 'url("../resources/images/greyMinus.png")');
-          } else {
-              $(this).css('list-style-image', 'url("../resources/images/greyPlus.png")');
-          }
-          $(this).find(".usersOnlineTableContainer").toggle();
-      }
-  );
+function hiddenBlocks(param){
+    $(param).click(
+        function() {
+            if ($(this).find(".usersOnlineTableContainer").css("display") == 'none') {
+             
+                $(this).css('list-style', 'none');
+            //              $(this).css('list-style-image', 'url("../resources/images/greyMinus.png")');
+            //              $(this).css('list-style-position', 'inside;');
+            } else {
+                //              $(this).css('list-style-image', 'url("../resources/images/greyPlus.png")');
+                $(this).css('list-style', 'none');
+            }
+            $(this).find(".usersOnlineTableContainer").toggle();
+        }
+        );
 }
