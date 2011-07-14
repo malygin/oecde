a9e8d21ffe4062894ca0a3dc7989825f91254901
Латынская Анастасия Алексеевна <latunskayaaa@info.sgu.ru>
2011-07-14 13:32:17 +0000

    скрывающиеся блоки подсказки
    git-svn-id: file:///var/www/svn2/oecdo/oecde@1182 50b15a66-aedb-45cf-97b2-84293224aabc

--- web/src/main/webapp/resources/scripts/usersOn-line.js
+++ web/src/main/webapp/resources/scripts/usersOn-line.js
@@ -1,21 +1,39 @@
-$(document).ready(function(){
-    hiddenBlocks("ul.dropdown li");
-});
+//$(document).ready(function(){
+//    hiddenBlocks(".clickToHid");
+//});
+//
+//
+//function hiddenBlocks(param){
+//    $(param).click(
+//        function() {
+//            if ($(this).parent().find(".hiddenUsersOnlineTableContainer").css("display") == 'none') {
+//
+////                $(this).parent().css('list-style', 'none');
+//                      $(this).parent().css('list-style-image', 'url("../resources/images/greyMinus.png")');
+//            } else {
+//                            $(this).parent().css('list-style-image', 'url("../resources/images/greyPlus.png")');
+////                $(this).parent().css('list-style', 'none');
+//            }
+//            $(this).parent().find(".hiddenUsersOnlineTableContainer").toggle();
+//        }
+//        );
+//}
+ function toHidElements (hiddenElement){
+           $("*[class*='clickToHid']").click(
+           function(){
+               var hiddenElem = $(this).parent().find(hiddenElement);
+               $(hiddenElem).toggle();
+               if(hiddenElement == '.hiddenUsersOnlineTableContainer'){
+                    if ($(hiddenElem).css("display") == 'none') {
 
-
-function hiddenBlocks(param){
-    $(param).click(
-        function() {
-            if ($(this).find(".usersOnlineTableContainer").css("display") == 'none') {
-             
-                $(this).css('list-style', 'none');
-            //              $(this).css('list-style-image', 'url("../resources/images/greyMinus.png")');
-            //              $(this).css('list-style-position', 'inside;');
-            } else {
-                //              $(this).css('list-style-image', 'url("../resources/images/greyPlus.png")');
-                $(this).css('list-style', 'none');
+//                $(this).parent().css('list-style', 'none');
+                      $(hiddenElem).parent().css('list-style-image', 'url("../resources/images/greyPlus.png")');
+                    } else {
+                      $(hiddenElem).parent().css('list-style-image', 'url("../resources/images/greyMinus.png")');
+//                $(this).parent().css('list-style', 'none');
             }
-            $(this).find(".usersOnlineTableContainer").toggle();
-        }
-        );
-}
+               }
+           }
+        )};
+
+
