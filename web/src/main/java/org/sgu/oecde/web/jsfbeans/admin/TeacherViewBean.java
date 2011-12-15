c86e3bafe5b75114140571b0c9e6598b1c4a1102
Malygin Andrey <malygin@info.sgu.ru>
2011-12-15 13:45:28 +0000

    информация об учителях
    git-svn-id: file:///var/www/svn2/oecdo/oecde@1436 50b15a66-aedb-45cf-97b2-84293224aabc

--- web/src/main/java/org/sgu/oecde/web/jsfbeans/admin/TeacherViewBean.java
+++ web/src/main/java/org/sgu/oecde/web/jsfbeans/admin/TeacherViewBean.java
@@ -1,13 +1,15 @@
 package org.sgu.oecde.web.jsfbeans.admin;
 
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import org.sgu.oecde.core.users.AbstractUser;
 import org.sgu.oecde.core.users.Teacher;
 import org.sgu.oecde.web.jsfbeans.UserViewBean;
 import org.sgu.oecde.web.jsfbeans.teacher.TeacherIndexBean;
 import org.sgu.oecde.web.jsfbeans.teacher.TeacherSessionBean;
-
+import org.sgu.oecde.de.users.Group;
 /**
  *
  * @author ShihovMY
@@ -23,11 +25,29 @@ public class TeacherViewBean extends UserViewBean{
     private TeacherIndexBean teacherIndexBean;
 
     private static final long serialVersionUID = 172L;
+    
+    private List<Group> groups;
+    private  Integer semester;
+    protected AbstractUser user;
+    
+    public Integer getSemester() {
+        return semester;
+    }
 
+    public void setSemester(Integer semester) {
+        this.semester = semester;
+    }
+    
+    
+     public TeacherViewBean() {
+         setType("TEACHER");
+    }
+     
     @Override
     public void setId(Long id) {
         super.setId(id);
-        teacherSessionBean.setTeacher((Teacher) user);
+        getUser();
+        groups = teacherSessionBean.getGroups(semester);      
         teacherIndexBean.setTeacher((Teacher) user);
     }
 
@@ -35,7 +55,31 @@ public class TeacherViewBean extends UserViewBean{
         this.teacherIndexBean = teacherIndexBean;
     }
 
+
     public void setTeacherSessionBean(TeacherSessionBean teacherSessionBean) {
         this.teacherSessionBean = teacherSessionBean;
+    }
+    @Override
+    public AbstractUser getUser() {
+        if(user==null){
+            user = super.getUser();
+            teacherSessionBean.setTeacher((Teacher) user);
+            teacherIndexBean.setTeacher((Teacher) user);
+        }
+        return user;
+    }
+    public List<Group> getTeacherGroups(){
+            return groups;
+    }
+    public int getGroupsCount(){
+        return  groups.size();
+    }
+    
+    public int getStudentsCount(){
+        int count=0;
+        for(Group group:  groups){
+            count += group.getPersons().size();
+        }
+        return count;
     }
 }
